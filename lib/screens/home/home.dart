0604fbe636bd7de01031651a24f2eaed26845788
mrtvirus <mrtvirus@kali.mrtvirushub.com>
2020-10-22 13:45:03 +0600

    logo added

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kenakata/constants.dart';
 import 'package:kenakata/screens/home/components/body.dart';
 
@@ -17,12 +18,22 @@ AppBar buildAppBar() {
   return AppBar(
     backgroundColor: Colors.white,
     elevation: 0,
-    leading: IconButton(
-      icon: Icon(Icons.notifications_rounded),
-      color: kTextColor,
-      onPressed: () {},
+    leading: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(
+          "assets/images/logo.svg",
+          fit: BoxFit.contain,
+          height: 30,
+        ),
+      ],
     ),
     actions: <Widget>[
+      IconButton(
+        icon: Icon(Icons.notifications_rounded),
+        color: kTextColor,
+        onPressed: () {},
+      ),
       IconButton(
         icon: Icon(Icons.add_shopping_cart_rounded),
         color: kTextColor,
